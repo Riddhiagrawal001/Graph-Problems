9fe7dc4452d17dd75f20b92e47d85cc5062495cc
Riddhiagrawal001 <50551695+Riddhiagrawal001@users.noreply.github.com>
2022-06-02 15:53:40 +0530

    Rat in a Maze Problem - I
    Consider a rat placed at (0, 0) in a square matrix of order N * N. It has to reach the destination at (N - 1, N - 1). Find all possible paths that the rat can take to reach from source to destination. The directions in which the rat can move are 'U'(up), 'D'(down), 'L' (left), 'R' (right). Value 0 at a cell in the matrix represents that it is blocked and rat cannot move to it while value 1 at a cell in the matrix represents that rat can be travel through it.
    Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell.

---
+++ Rat in a Maze Problem - I.java
@@ -0,0 +1,48 @@
+/* the idea is to go to everywhere form the current position and then if there is a blockage then
+backtrack to the place making the visited as unvisited */
+
+
+
+class Solution {
+    static void dfs(int i,int j, int[][] m ,ArrayList<String> res, boolean[][] visited,String s,int n){
+      // outer boundries   
+      if(i<0 || j<0 || i>=n || j>=n) return;
+      // if the next cell is 0 or it is visited beforehand 
+        if(m[i][j]==0 || visited[i][j]==true) return;
+      
+      // if we reach to the destination
+        if(i==n-1 && j==n-1 ) {
+            res.add(s);
+            return;
+        }
+        visited[i][j]=true;
+      
+      // moving to everyplace from the current cell
+        //up
+        dfs(i-1,j,m,res,visited,s+'U',n);
+        //down
+         dfs(i+1,j,m,res,visited,s+'D',n);
+        //left
+        dfs(i,j-1,m,res,visited,s+'L',n);
+        //right
+         dfs(i,j+1,m,res,visited,s+'R',n);
+         
+         visited[i][j]=false;
+        
+    }
+    public static ArrayList<String> findPath(int[][] m, int n) {
+        // Your code here
+        ArrayList<String> res=new ArrayList<>();
+        boolean[][] visited=new boolean[n][n];
+        for(boolean[] r: visited)
+           Arrays.fill(r,false);
+        String s="";   
+        if(m[0][0]==0 || m[n-1][n-1]==0){
+           s=Integer.toString(-1);
+           res.add(s);
+        }  
+            dfs(0,0,m,res,visited,s,n);
+            Collections.sort(res);
+        return res;
+    }
+}
